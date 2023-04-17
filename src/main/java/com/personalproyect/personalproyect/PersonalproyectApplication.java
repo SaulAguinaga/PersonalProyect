abef7ab55d7f3aee1d9d53a17595085623d5bcae
SaulAguinaga <saul_aguinaga@hotmail.es>
2023-04-17 16:17:32 +0200

    first commit

---
+++ src/main/java/com/personalproyect/personalproyect/PersonalproyectApplication.java
@@ -0,0 +1,13 @@
+package com.personalproyect.personalproyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalproyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalproyectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personalproyect/personalproyect/PersonalproyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalproyect.personalproyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalproyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
